44e4877858be6a9c29baf2b529f9dcfe8a31e8f8
FursAleksey <31897439+FursAleksey@users.noreply.github.com>
2019-01-27 00:19:55 +0300

    fix: change directory

--- src/components/ProjectTeam/MemberOfTeam/MemberOfTeam.js
+++ src/components/ProjectTeam/MemberOfTeam/MemberOfTeam.js
@@ -4,7 +4,7 @@ import './MemberOfTeam.css';
 function MemberOfTeam(props) {
   return (
     <div className='memberOfTeam'>
-      <img src={props.data.photo} alt={props.data.name} width='80px' height='80px'/>
+      <img src={props.data.photo} alt={props.data.name} width='60px' height='60px'/>
       <h3>{props.data.name}
         <span>(@<a href={props.data.githubURL} target='_blank' rel='noopener noreferrer'>{props.data.nickName}</a>)</span>
       </h3>
