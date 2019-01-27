3c436999e2e746550658c4e1c016b044d6126124
FursAleksey <31897439+FursAleksey@users.noreply.github.com>
2019-01-27 12:40:19 +0300

    fix: use url instead of import

--- src/components/ProjectTeam/contributors/contributors.js
+++ src/components/ProjectTeam/contributors/contributors.js
@@ -1,36 +1,30 @@
-import aborisenokPhoto from '../photos/aborisenok.jpg';
-import youngbrickPhoto from '../photos/youngbrick.png';
-import kevinkPhoto from '../photos/2kevink.jpg';
-import fursalekseyPhoto from '../photos/fursaleksey.png';
-import hentmirePhoto from '../photos/hentmire.jpg';
-
 const contributors = {
   aborisenok: {
-    photo: aborisenokPhoto,
+    photo: '../photos/aborisenok.jpg',
     name: 'Aliaksei Barysionak',
     nickName: 'aborisenok',
     githubURL: 'https://github.com/aborisenok'
   },
   youngbrick: {
-    photo: youngbrickPhoto,
+    photo: '../photos/youngbrick.png',
     name: 'Nastassia Krasheuskaya',
     nickName: 'youngbrick',
     githubURL: 'https://github.com/youngbrick'
   },
   kevink: {
-    photo: kevinkPhoto,
+    photo: '../photos/2kevink.jpg',
     name: 'Dzmitryi Kestner',
     nickName: '2kevink',
     githubURL: 'https://github.com/2kevink'
   },
   fursaleksey: {
-    photo: fursalekseyPhoto,
+    photo: '../photos/fursaleksey.png',
     name: 'Aliaksei Furs',
     nickName: 'fursaleksey',
     githubURL: 'https://github.com/fursaleksey'
   },
   hentmire: {
-    photo: hentmirePhoto,
+    photo: '../photos/hentmire.jpg',
     name: 'Viktoryia Udodava',
     nickName: 'hentmire',
     githubURL: 'https://github.com/hentmire'
