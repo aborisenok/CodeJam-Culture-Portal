3d377d6b2009e3a6aa535e3a765b1e90980a63e3
YoungBrick <tidorka@gmail.com>
2019-01-27 15:13:50 +0300

    feat: create TimeLine component, add export to people.js

---
+++ src/components/TimeLine/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+
+export default function TimeLine (props) {
+	return (
+		<Timeline lineColor={'#ddd'}>
+			{props.activity.map((item) => {
+				return (
+					<TimelineItem
+						dateText={item.date}
+						dateInnerStyle={{ background: '#474747', color: '#fff' }}
+					>
+						<h3>{item.place}</h3>
+						<p>{item.description}</p>
+					</TimelineItem>
+				)
+			}
+			)}
+		</Timeline>
+	)
+}
