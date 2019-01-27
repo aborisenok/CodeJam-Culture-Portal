7b2b463dc2c140e4bef79e55d310c13d2af3cfbc
FursAleksey <31897439+FursAleksey@users.noreply.github.com>
2019-01-27 12:35:11 +0300

    feat: add ProjectDescription Component

---
+++ src/components/ProjectDescription/ProjectDescription.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProjectDescription.css';
+
+function ProjectDescription() {
+  return (
+    <div className='projectDescription'>
+      <p>
+        Добро пожаловать на портал, посвященный фотографам Беларуси. Здесь можно найти информацию о фотографе и вдохновиться его работами.
+      </p>
+    </div>
+  )
+}
+
+export default ProjectDescription;
